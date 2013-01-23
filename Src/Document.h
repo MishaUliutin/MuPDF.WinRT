3c3df9173c9463c08f9b0de7a7c95b0833042a4b
unknown <mishaulyutin@gmail.com>
2013-01-23 11:44:01 +0200

    style fix

--- Src/Document.h
+++ Src/Document.h
@@ -1,22 +1,22 @@
 ﻿#pragma once
 
 extern "C" {
-#include "mupdf.h"
+	#include "mupdf.h"
 }
 
 namespace MuPDFWinRT
 {
-    public ref class Document sealed
-    {
+	public ref class Document sealed
+	{    
 	private:
 		fz_context *m_context;
 		fz_document *m_document;
 		Document();
 		void Init(Windows::Storage::Streams::IBuffer^ buffer);
 		void InitContext();
-		void InitDocument(Windows::Storage::Streams::IBuffer^ buffer);
-    public:
+		void InitDocument(Windows::Storage::Streams::IBuffer^ buffer);	
+	public:
 		static Document^ Create(Windows::Storage::Streams::IBuffer^ buffer);
 		virtual ~Document();
-    };
+	};
 }
