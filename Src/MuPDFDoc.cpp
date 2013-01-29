53b2f02b362f15501252522ffdcbbc487e83dfed
unknown <mishaulyutin@gmail.com>
2013-01-29 18:29:44 +0200

    add event callback to wrapper

--- Src/MuPDFDoc.cpp
+++ Src/MuPDFDoc.cpp
@@ -3,7 +3,7 @@
 #include "MuPDFDoc.h"
 
 MuPDFDoc::MuPDFDoc(int resolution)
-	: m_context(nullptr), m_document(nullptr), m_resolution(resolution), m_currentPage(-1)
+	: m_context(nullptr), m_document(nullptr), m_resolution(resolution), m_currentPage(-1)/*, m_alertEventCallback(nullptr)*/
 {
 	for(int i = 0; i < NUM_CACHE; i++)
 	{
@@ -33,6 +33,20 @@ HRESULT MuPDFDoc::Create(unsigned char *buffer, int bufferLen, const char *mimeT
 	}
 }
 
+//HRESULT MuPDFDoc::Create(unsigned char *buffer, int bufferLen, const char *mimeType, int resolution, AlertEventCallback alertEventCallback, MuPDFDoc **obj)
+//{
+//	HRESULT result = Create(buffer, bufferLen, mimeType, resolution, obj);
+//	if (FAILED(result))
+//	{
+//		return result;
+//	}
+//	else
+//	{
+//		(*obj)->m_alertEventCallback = alertEventCallback;
+//		return S_OK;
+//	}
+//}
+//
 MuPDFDoc::~MuPDFDoc()
 {
 	if (m_outline)
@@ -69,7 +83,7 @@ HRESULT MuPDFDoc::Init(unsigned char *buffer, int bufferLen, const char *mimeTyp
 
 HRESULT MuPDFDoc::InitContext()
 {
-	m_context = fz_new_context(NULL, NULL, FZ_STORE_DEFAULT);
+	m_context = fz_new_context(nullptr, nullptr, FZ_STORE_DEFAULT);
 	if (!m_context)
 	{
 		return E_OUTOFMEMORY;
@@ -79,6 +93,112 @@ HRESULT MuPDFDoc::InitContext()
 		return S_OK;
 	}
 }
+//
+//static MuPDFAlertIconType GetMuPDFAlertIconType(int iconType)
+//{
+//	switch (iconType)
+//	{
+//	case FZ_ALERT_ICON_ERROR:
+//		return ALERTICONERROR;
+//	case FZ_ALERT_ICON_WARNING:
+//		return ALERTICONWARNING;
+//	case FZ_ALERT_ICON_QUESTION:
+//		return ALERTICONQUESTION;
+//	case FZ_ALERT_ICON_STATUS:
+//		return ALERTICONSTATUS;
+//	default:
+//		//!!!
+//		return (MuPDFAlertIconType)iconType;
+//	};
+//}
+//
+//static MuPDFAlertButtonGroupType GetMuPDFAlertButtonGroupType(int buttonGroupType)
+//{
+//	switch (buttonGroupType)
+//	{
+//	case FZ_ALERT_BUTTON_GROUP_OK:
+//		return ALERTBUTTONGROUPOK;
+//	case FZ_ALERT_BUTTON_GROUP_OK_CANCEL:
+//		return ALERTBUTTONGROUPOKCANCEL;
+//	case FZ_ALERT_BUTTON_GROUP_YES_NO:
+//		return ALERTBUTTONGROUPYESNO;
+//	case FZ_ALERT_BUTTON_GROUP_YES_NO_CANCEL:
+//		return ALERTBUTTONGROUPYESNOCANCEL;
+//	default:
+//		//!!!
+//		return (MuPDFAlertButtonGroupType)buttonGroupType;
+//	}
+//}
+//
+//static int GetButtonPressed(MuPDFAlertButtonPressed buttonPressed)
+//{
+//	switch (buttonPressed)
+//	{
+//	case ALERTBUTTONNONE:
+//		return FZ_ALERT_BUTTON_NONE;
+//	case ALERTBUTTONOK:
+//		return FZ_ALERT_BUTTON_OK;
+//	case ALERTBUTTONCANCEL:
+//		return FZ_ALERT_BUTTON_CANCEL;
+//	case ALERTBUTTONNO:
+//		return FZ_ALERT_BUTTON_NO;
+//	case ALERTBUTTONYES:
+//		return FZ_ALERT_BUTTON_YES;
+//	default:
+//		return (int)buttonPressed;
+//	}
+//}
+
+static std::unique_ptr<char[]> CopyToUniqueStr(char *str)
+{
+	size_t len = strlen(str);
+	std::unique_ptr<char[]> copyStr(new char[len + 1]);
+	strcpy_s(copyStr.get(), len + 1, str);
+	return copyStr;
+}
+
+//static std::shared_ptr<MuPDFAlertEvent> CreateMuPDFAlertEvent(const fz_alert_event* alert)
+//{
+//	std::shared_ptr<MuPDFAlertEvent> alertEvent(new MuPDFAlertEvent());
+//	alertEvent->message = CopyToUniqueStr(alert->message);
+//	alertEvent->iconType = GetMuPDFAlertIconType(alert->icon_type);
+//	alertEvent->buttonGroupType = GetMuPDFAlertButtonGroupType(alert->button_group_type);
+//	alertEvent->title = CopyToUniqueStr(alert->title);
+//	alertEvent->checkBoxMessage = CopyToUniqueStr(alert->check_box_message);
+//	alertEvent->initiallyChecked = alert->initially_checked == 0 ? false : true;
+//	return alertEvent;
+//}
+//
+//void MuPDFDoc::ShowAlert(fz_alert_event* alert)
+//{
+//	std::shared_ptr<MuPDFAlertEvent> alertEvent = CreateMuPDFAlertEvent(alert);
+//	if (m_alertEventCallback)
+//	{
+//		m_alertEventCallback(alertEvent);
+//		alert->finally_checked = alertEvent->finallyChecked;
+//		alert->button_pressed = GetButtonPressed(alertEvent->buttonPressed);		
+//	}
+//}
+//
+//void MuPDFDoc::EventCallback(fz_doc_event* event, void* data)
+//{
+//	MuPDFDoc* doc = (MuPDFDoc*)data;
+//	
+//	switch (event->type)
+//	{
+//	case FZ_DOCUMENT_EVENT_ALERT:
+//		doc->ShowAlert(fz_access_alert_event(event));
+//		break;
+//	}
+//}
+//
+//void MuPDFDoc::AlertsInit()
+//{
+//	fz_interactive* idoc = fz_interact(m_document);
+//	if (!idoc)
+//		return;
+//	fz_set_doc_event_callback(idoc, EventCallback, this);
+//}
 
 HRESULT MuPDFDoc::InitDocument(unsigned char *buffer, int bufferLen, const char *mimeType)
 {
@@ -93,7 +213,7 @@ HRESULT MuPDFDoc::InitDocument(unsigned char *buffer, int bufferLen, const char 
 		{
 			m_document = fz_open_document_with_stream(m_context, mimeType, stream);
 			m_outline = fz_load_outline(m_document);
-			//TODO: alerts_init(glo);
+			//AlertsInit();
 		}
 		fz_catch(m_context)
 		{
@@ -128,13 +248,13 @@ void MuPDFDoc::ClearPages()
 void MuPDFDoc::ClearPageCache(PageCache *pageCache)
 {
 	fz_free_display_list(m_context, pageCache->pageList);
-	pageCache->pageList = NULL;
+	pageCache->pageList = nullptr;
 	fz_free_display_list(m_context, pageCache->annotList);
-	pageCache->annotList = NULL;
+	pageCache->annotList = nullptr;
 	fz_free_page(m_document, pageCache->page);
-	pageCache->page = NULL;
+	pageCache->page = nullptr;
 	fz_free_page(m_document, pageCache->hqPage);
-	pageCache->hqPage = NULL;
+	pageCache->hqPage = nullptr;
 }
 
 int MuPDFDoc::FindPageInCache(int pageNumber)
@@ -197,8 +317,7 @@ HRESULT MuPDFDoc::GotoPage(int pageNumber)
 		pageCache->page = fz_load_page(m_document, pageCache->number);
 		pageCache->mediaBox = fz_bound_page(m_document, pageCache->page);
 		// fz_bound_page determine the size of a page at 72 dpi.
-		float zoom = m_resolution / 72.0;
-		fz_matrix ctm = fz_scale(zoom, zoom);
+		fz_matrix ctm = CalcConvertMatrix();
 		fz_bbox bbox = fz_round_rect(fz_transform_rect(ctm, pageCache->mediaBox));
 		pageCache->width = bbox.x1-bbox.x0;
 		pageCache->height = bbox.y1-bbox.y0;
@@ -236,8 +355,8 @@ bool MuPDFDoc::AuthenticatePassword(char *password)
 
 HRESULT MuPDFDoc::DrawPage(unsigned char *bitmap, int x, int y, int width, int height, bool invert)
 {
-	fz_device *dev = NULL;
-	fz_pixmap *pixmap = NULL;
+	fz_device *dev = nullptr;
+	fz_pixmap *pixmap = nullptr;
 	fz_var(dev);
 	fz_var(pixmap);
 	PageCache *pageCache = &m_pages[m_currentPage];
@@ -254,19 +373,19 @@ HRESULT MuPDFDoc::DrawPage(unsigned char *bitmap, int x, int y, int width, int h
 			/* Render to list */
 			pageCache->pageList = fz_new_display_list(m_context);
 			dev = fz_new_list_device(m_context, pageCache->pageList);
-			fz_run_page_contents(m_document, pageCache->page, dev, fz_identity, NULL);
+			fz_run_page_contents(m_document, pageCache->page, dev, fz_identity, nullptr);
 		}
 		if (!pageCache->annotList)
 		{
 			if (dev)
 			{
 				fz_free_device(dev);
-				dev = NULL;
+				dev = nullptr;
 			}
 			pageCache->annotList = fz_new_display_list(m_context);
 			dev = fz_new_list_device(m_context, pageCache->annotList);
 			for (fz_annot *annot = fz_first_annot(m_document, pageCache->page); annot; annot = fz_next_annot(m_document, annot))
-				fz_run_annot(m_document, pageCache->page, annot, dev, fz_identity, NULL);
+				fz_run_annot(m_document, pageCache->page, annot, dev, fz_identity, nullptr);
 		}
 		fz_bbox rect;
 		rect.x0 = x;
@@ -281,8 +400,7 @@ HRESULT MuPDFDoc::DrawPage(unsigned char *bitmap, int x, int y, int width, int h
 		}
 		fz_clear_pixmap_with_value(m_context, pixmap, 0xff);
 		//
-		float zoom = m_resolution / 72.0;
-		fz_matrix ctm = fz_scale(zoom, zoom);
+		fz_matrix ctm = CalcConvertMatrix();
 		fz_bbox bbox = fz_round_rect(fz_transform_rect(ctm, pageCache->mediaBox));
 		/* Now, adjust ctm so that it would give the correct page width
 		 * heights. */
@@ -293,15 +411,15 @@ HRESULT MuPDFDoc::DrawPage(unsigned char *bitmap, int x, int y, int width, int h
 		if (dev)
 		{
 			fz_free_device(dev);
-			dev = NULL;
+			dev = nullptr;
 		}
 		dev = fz_new_draw_device(m_context, pixmap);
 		if (pageCache->pageList)
-			fz_run_display_list(pageCache->pageList, dev, ctm, bbox, NULL);
+			fz_run_display_list(pageCache->pageList, dev, ctm, bbox, nullptr);
 		if (pageCache->annotList)
-			fz_run_display_list(pageCache->annotList, dev, ctm, bbox, NULL);
+			fz_run_display_list(pageCache->annotList, dev, ctm, bbox, nullptr);
 		fz_free_device(dev);
-		dev = NULL;
+		dev = nullptr;
 		if (invert)
 			fz_invert_pixmap(m_context, pixmap);
 		fz_drop_pixmap(m_context, pixmap);
@@ -311,7 +429,7 @@ HRESULT MuPDFDoc::DrawPage(unsigned char *bitmap, int x, int y, int width, int h
 		if (dev)
 		{
 			fz_free_device(dev);
-			dev = NULL;
+			dev = nullptr;
 		}
 		if (pixmap)
 		{
@@ -334,8 +452,8 @@ HRESULT MuPDFDoc::UpdatePage(int pageNumber, unsigned char *bitmap, int x, int y
 		}
 		return DrawPage(bitmap, x, y, width, height, invert);
 	}
-	fz_device *dev = NULL;
-	fz_pixmap *pixmap = NULL;
+	fz_device *dev = nullptr;
+	fz_pixmap *pixmap = nullptr;
 	fz_var(dev);
 	fz_var(pixmap);
 	PageCache *pageCache = &m_pages[m_currentPage];
@@ -352,19 +470,19 @@ HRESULT MuPDFDoc::UpdatePage(int pageNumber, unsigned char *bitmap, int x, int y
 			/* Render to list */
 			pageCache->pageList = fz_new_display_list(m_context);
 			dev = fz_new_list_device(m_context, pageCache->pageList);
-			fz_run_page_contents(m_document, pageCache->page, dev, fz_identity, NULL);
+			fz_run_page_contents(m_document, pageCache->page, dev, fz_identity, nullptr);
 		}
 		if (!pageCache->annotList)
 		{
 			if (dev)
 			{
 				fz_free_device(dev);
-				dev = NULL;
+				dev = nullptr;
 			}
 			pageCache->annotList = fz_new_display_list(m_context);
 			dev = fz_new_list_device(m_context, pageCache->annotList);
 			for (fz_annot *annot = fz_first_annot(m_document, pageCache->page); annot; annot = fz_next_annot(m_document, annot))
-				fz_run_annot(m_document, pageCache->page, annot, dev, fz_identity, NULL);
+				fz_run_annot(m_document, pageCache->page, annot, dev, fz_identity, nullptr);
 		}
 		fz_bbox rect;
 		rect.x0 = x;
@@ -373,8 +491,7 @@ HRESULT MuPDFDoc::UpdatePage(int pageNumber, unsigned char *bitmap, int x, int y
 		rect.y1 = y + height;
 		pixmap = fz_new_pixmap_with_bbox_and_data(m_context, fz_device_rgb, rect, bitmap);
 		//
-		float zoom = m_resolution / 72.0;
-		fz_matrix ctm = fz_scale(zoom, zoom);
+		fz_matrix ctm = CalcConvertMatrix();
 		fz_bbox bbox = fz_round_rect(fz_transform_rect(ctm, pageCache->mediaBox));
 		/* Now, adjust ctm so that it would give the correct page width
 		 * heights. */
@@ -385,7 +502,7 @@ HRESULT MuPDFDoc::UpdatePage(int pageNumber, unsigned char *bitmap, int x, int y
 		if (dev)
 		{
 			fz_free_device(dev);
-			dev = NULL;
+			dev = nullptr;
 		}
 		fz_annot *annot;
 		while (idoc && (annot = fz_poll_changed_annot(idoc, pageCache->page)) != NULL)
@@ -398,11 +515,11 @@ HRESULT MuPDFDoc::UpdatePage(int pageNumber, unsigned char *bitmap, int x, int y
 				fz_clear_pixmap_rect_with_value(m_context, pixmap, 0xff, abox);
 				dev = fz_new_draw_device_with_bbox(m_context, pixmap, abox);
 				if (pageCache->pageList)
-					fz_run_display_list(pageCache->pageList, dev, ctm, abox, NULL);
+					fz_run_display_list(pageCache->pageList, dev, ctm, abox, nullptr);
 				if (pageCache->annotList)
-					fz_run_display_list(pageCache->annotList, dev, ctm, abox, NULL);
+					fz_run_display_list(pageCache->annotList, dev, ctm, abox, nullptr);
 				fz_free_device(dev);
-				dev = NULL;
+				dev = nullptr;
 				if (invert)
 					fz_invert_pixmap_rect(pixmap, abox);
 			}
@@ -414,7 +531,7 @@ HRESULT MuPDFDoc::UpdatePage(int pageNumber, unsigned char *bitmap, int x, int y
 		if (dev)
 		{
 			fz_free_device(dev);
-			dev = NULL;
+			dev = nullptr;
 		}
 		if (pixmap)
 		{
@@ -444,14 +561,6 @@ HRESULT MuPDFDoc::UpdatePage(int pageNumber, unsigned char *bitmap, int x, int y
 //}
 //
 
-static std::unique_ptr<char[]> CopyUniqueStr(char *str)
-{
-	size_t len = strlen(str);
-	std::unique_ptr<char[]> copyStr(new char[len + 1]);
-	strcpy_s(copyStr.get(), len + 1, str);
-	return copyStr;
-}
-
 int MuPDFDoc::FillOutline(
 	std::shared_ptr<std::vector<std::shared_ptr<Outlineitem>>> items, 
 	int position, 
@@ -468,7 +577,7 @@ int MuPDFDoc::FillOutline(
 				std::shared_ptr<Outlineitem> item(new Outlineitem());
 				item->level = level;
 				item->pageNumber = pageNumber;
-				item->title = CopyUniqueStr(outline->title);
+				item->title = CopyToUniqueStr(outline->title);
 				items->push_back(item); 
 				position++;
 			}
@@ -512,7 +621,7 @@ static std::shared_ptr<MuPDFDocLink> CreateRemoteLink(const fz_link *link, fz_re
 	docLink->type = REMOTE;
 	docLink->remotePageNumber = link->dest.ld.gotor.page;
 	docLink->newWindow = link->dest.ld.gotor.new_window != 0 ? true : false;
-	docLink->fileSpec = CopyUniqueStr(link->dest.ld.gotor.file_spec);
+	docLink->fileSpec = CopyToUniqueStr(link->dest.ld.gotor.file_spec);
 	return docLink;
 }
 
@@ -520,7 +629,7 @@ std::shared_ptr<MuPDFDocLink> CreateURILink(const fz_link *link, fz_rect &rect)
 {
 	std::shared_ptr<MuPDFDocLink> docLink(CreateLink(rect));
 	docLink->type = URI;
-	docLink->uri = CopyUniqueStr(link->dest.ld.uri.uri);	
+	docLink->uri = CopyToUniqueStr(link->dest.ld.uri.uri);	
 	return docLink;
 }
 
@@ -528,8 +637,7 @@ std::shared_ptr<std::vector<std::shared_ptr<MuPDFDocLink>>> MuPDFDoc::GetLinks()
 {
 	PageCache *pageCache = &m_pages[m_currentPage];
 	std::shared_ptr<std::vector<std::shared_ptr<MuPDFDocLink>>> links(new std::vector<std::shared_ptr<MuPDFDocLink>>());
-	float zoom = m_resolution / 72.0;
-	fz_matrix ctm = fz_scale(zoom, zoom);
+	fz_matrix ctm = CalcConvertMatrix();
 	fz_link* list = fz_load_links(m_document, pageCache->page);
 	for(fz_link* link = list; link; link = link->next)
 	{
@@ -563,3 +671,56 @@ std::shared_ptr<std::vector<std::shared_ptr<MuPDFDocLink>>> MuPDFDoc::GetLinks()
 	fz_drop_link(m_context, list);
 	return links;
 }
+
+fz_matrix MuPDFDoc::CalcConvertMatrix()
+{
+	// fz_bound_page determine the size of a page at 72 dpi.
+	float zoom = m_resolution / 72.0;
+	return fz_scale(zoom, zoom);
+}
+
+//void MuPDFDoc::DumpAnnotationDisplayLists()
+//{
+//	for (int i = 0; i < NUM_CACHE; i++)
+//	{
+//		if (m_pages[i].annotList)
+//		{
+//			fz_free_display_list(m_context, m_pages[i].annotList);
+//			m_pages[i].annotList = nullptr;
+//		}
+//	}
+//}
+//
+//HRESULT MuPDFDoc::PassClickEvent(float x, float y, bool& changed)
+//{
+//	changed = false;
+//	fz_interactive *idoc = fz_interact(m_document);
+//	if (!idoc)
+//		return S_OK;
+//	PageCache *pageCache = &m_pages[m_currentPage];	
+//	fz_matrix ctm = CalcConvertMatrix();
+//	ctm = fz_invert_matrix(ctm);
+//	fz_point p;
+//	p.x = x;
+//	p.y = y;
+//	p = fz_transform_point(ctm, p);
+//	fz_try(m_context)
+//	{
+//		fz_ui_event event;
+//		event.etype = FZ_EVENT_TYPE_POINTER;
+//		event.event.pointer.pt = p;
+//		event.event.pointer.ptype = FZ_POINTER_DOWN;
+//		changed = fz_pass_event(idoc, pageCache->page, &event) == 0 ? false : true;
+//		event.event.pointer.ptype = FZ_POINTER_UP;
+//		changed |= fz_pass_event(idoc, pageCache->page, &event) == 0 ? false : true;
+//		if (changed) 
+//		{
+//			DumpAnnotationDisplayLists();
+//		}
+//	}
+//	fz_catch(m_context)
+//	{
+//		return E_FAIL;
+//	}
+//	return S_OK;
+//}
--- Src/MuPDFDoc.h
+++ Src/MuPDFDoc.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <memory>
+#include <functional>
 #include <vector>
 #include <Winerror.h>
 
@@ -33,6 +34,22 @@ typedef struct
 	int pageNumber;
 } Outlineitem;
 
+template<class T>
+struct Rect
+{
+    Rect() : left(), top(), right(), bottom() {}
+    Rect(T left, T top, T right, T bottom) : 
+        left(left), top(top), right(right), bottom(bottom) {}
+
+    T left;
+    T top;
+    T right;
+    T bottom;
+};
+
+typedef Rect<float> RectFloat;
+
+/* Possible values of link type */
 enum MuPDFDocLinkType { INTERNAL, URI, REMOTE };
 
 typedef struct
@@ -52,6 +69,29 @@ typedef struct
 	bool newWindow;	
 } MuPDFDocLink;
 
+///* Possible values of icon type */
+//enum  MuPDFAlertIconType { ALERTICONERROR, ALERTICONWARNING, ALERTICONQUESTION, ALERTICONSTATUS };
+//
+///* Possible values of button group type */
+//enum MuPDFAlertButtonGroupType { ALERTBUTTONGROUPOK, ALERTBUTTONGROUPOKCANCEL, ALERTBUTTONGROUPYESNO, ALERTBUTTONGROUPYESNOCANCEL };
+//
+///* Possible values of button pressed */
+//enum MuPDFAlertButtonPressed { ALERTBUTTONNONE, ALERTBUTTONOK, ALERTBUTTONCANCEL, ALERTBUTTONNO, ALERTBUTTONYES };
+//
+//typedef struct
+//{
+//	std::unique_ptr<char[]> message;
+//	MuPDFAlertIconType iconType;
+//	MuPDFAlertButtonGroupType buttonGroupType;
+//	std::unique_ptr<char[]> title;
+//	std::unique_ptr<char[]> checkBoxMessage;
+//	bool initiallyChecked;
+//	bool finallyChecked;
+//	MuPDFAlertButtonPressed buttonPressed;
+//} MuPDFAlertEvent;
+//
+//typedef std::function<void (std::shared_ptr<MuPDFAlertEvent>)> AlertEventCallback;
+
 class MuPDFDoc
 {
 private:
@@ -60,6 +100,7 @@ private:
 	fz_outline *m_outline;
 	int m_currentPage;
 	int m_resolution;
+	//AlertEventCallback m_alertEventCallback;
 	PageCache m_pages[NUM_CACHE];
 	MuPDFDoc(int resolution);
 	HRESULT Init(unsigned char *buffer, int bufferLen, const char *mimeType);
@@ -70,11 +111,17 @@ private:
 	void ClearPages();
 	int FindPageInCache(int pageNumber);
 	int GetPageCacheIndex(int pageNumber);
+	fz_matrix CalcConvertMatrix();
 //	int CountOutlineitems(fz_outline *outline);
 	int FillOutline(std::shared_ptr<std::vector<std::shared_ptr<Outlineitem>>> items, int position, fz_outline *outline, int level);
+//	void DumpAnnotationDisplayLists();
+//	void ShowAlert(fz_alert_event* alert);
+//	void AlertsInit();
+//	static void EventCallback(fz_doc_event* event, void* data);
 //	void ClearHQPages();
 public:
 	static HRESULT Create(unsigned char *buffer, int bufferLen, const char *mimeType, int resolution, MuPDFDoc **obj);
+//	static HRESULT Create(unsigned char *buffer, int bufferLen, const char *mimeType, int resolution, AlertEventCallback alertEventCallback, MuPDFDoc **obj);
 	~MuPDFDoc(void);
 	HRESULT GotoPage(int pageNumber);
 	HRESULT DrawPage(unsigned char *bitmap, int x, int y, int width, int height, bool invert);
@@ -88,4 +135,5 @@ public:
 	int GetPageWidth();
 	int GetPageHeight();
 	std::shared_ptr<std::vector<std::shared_ptr<MuPDFDocLink>>> GetLinks();
+	//HRESULT PassClickEvent(float x, float y, bool& changed);
 };
